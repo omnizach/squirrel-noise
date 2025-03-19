81ebd080ad2a9021c15b56ad8bd1988a391491a6
Zach Young <1951403+omnizach@users.noreply.github.com>
2025-03-19 18:54:07 -0400

    created generator function

---
+++ src/lib/noiseGenerator.spec.ts
@@ -0,0 +1,22 @@
+import test from 'ava'
+import { noiseGenerator } from './noiseGenerator'
+
+test('noiseGenerator basic generator loop', (t) => {
+  let count = 0
+  for (const x of noiseGenerator()) {
+    count++
+
+    t.true(Number.isFinite(x))
+
+    if (count > 100) {
+      break
+    }
+  }
+})
+
+test('noiseGenerator materializes with finite length', t => {
+  const ns = [...noiseGenerator({ length: 50, range: [0, 10], discrete: true })]
+
+  t.log(ns)
+  t.is(ns.length, 50)
+})
---
+++ src/lib/noiseGenerator.ts
@@ -0,0 +1,20 @@
+import { NoiseOptions } from './noise'
+import { noiseNumber } from './noiseNumber'
+
+export interface NoiseGeneratorOptions {
+  length?: number
+}
+
+export function* noiseGenerator({
+  length = Infinity,
+  ...options
+}: Omit<NoiseOptions, 'dimensions'> &
+  NoiseGeneratorOptions = {}): Generator<number> {
+  const n = noiseNumber(options)
+
+  let i = 0
+
+  while (i < length) {
+    yield n(i++)
+  }
+}
